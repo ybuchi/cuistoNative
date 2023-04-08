48324e9ee001898a6967bf6986ba93ce9486ef76
ybuchi <yabuch_ch@hotmail.com>
2023-04-08 13:04:13 -0400

    Started building out home page.

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,9 +1,57 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, 
+        View, 
+        Text, 
+        TextInput, 
+        Image,
+        SafeAreaView
+     } from "react-native";
 
 
 export default function HomeScreen(){
     return(
-        <Text>Welcome to Cuisto</Text>
+        <View style={styles.padded}>
+            <View>
+                <Text style={styles.title}>Welcome to Cuisto</Text>
+                <Text style={styles.subtitle}>The app for home chefs.</Text>
+            </View>
+            <View style={styles.padded}>
+                <SafeAreaView>
+                    <TextInput style={styles.input} placeholder="Username"/>
+                    <TextInput style={styles.input} placeholder="Password"/>
+                </SafeAreaView>
+                
+            </View>
+            <View>
+                <Image style={styles.tinyLogo} source={require("/Users/Yannik/Cuisto/assets/CUISTO-splash.png")}/>
+            </View>
+           
+        </View>
+        
     )
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 25,
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    subtitle: {
+        fontSize: 15,
+        textAlign: "center" 
+    },
+    tinyLogo: {
+        width: 400,
+        height: 400
+    },
+    padded: {
+        padding: 20
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10
+    },
+})
